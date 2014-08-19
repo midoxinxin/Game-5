fda14063501c1f824c10f62589aca350243e8e64
wuhaolin <gwuhaolin@gmail.com>
2014-08-19 19:00:39 +0800

    游戏duoduo完成了微信接口

--- duoduo/app.js
+++ duoduo/app.js
@@ -213,18 +213,23 @@ function startGame() {
 var weixinData = {
 	"appid": 'wx5c6c73bc34fc424e',
 	"img_url": 'http://gwuhaolin.github.io/Game-5/duoduo/duoduo.png',
+	"img_width": "200",
+	"img_height": "200",
 	"link": 'http://gwuhaolin.github.io/Game-5/duoduo/index.html',
 	"desc": "找出颜色最大的方块,看看你的直觉有多准?",
 	"title": ""
 };
 function shareFriend() {
-	WeixinJSBridge.invoke('sendAppMessage', weixinData);
+	WeixinJSBridge.invoke('sendAppMessage', weixinData, function (res) {
+	});
 }
 function shareTimeline() {
-	WeixinJSBridge.invoke('sendAppMessage', weixinData);
+	WeixinJSBridge.invoke('sendAppMessage', weixinData, function (res) {
+	});
 }
 function shareWeibo() {
-	WeixinJSBridge.invoke('sendAppMessage', weixinData);
+	WeixinJSBridge.invoke('sendAppMessage', weixinData, function (res) {
+	});
 }
 // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
 document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
@@ -241,3 +246,44 @@ document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
 		shareWeibo();
 	});
 }, false);
+
+///////////////////////////////////
+//var imgUrl = 'http://xxx/share_ico.png';
+//var lineLink = 'http://xxx';
+//var descContent = "xx！";
+//var shareTitle = 'xx';
+//var appid = '';
+//
+//function shareFriend() {
+//	WeixinJSBridge.invoke('sendAppMessage', {
+//		"appid": appid,
+//		"img_url": imgUrl,
+//		"img_width": "200",
+//		"img_height": "200",
+//		"link": lineLink,
+//		"desc": descContent,
+//		"title": shareTitle
+//	}, function (res) {
+//		//_report('send_msg', res.err_msg);
+//	})
+//}
+//function shareTimeline() {
+//	WeixinJSBridge.invoke('shareTimeline', {
+//		"img_url": imgUrl,
+//		"img_width": "200",
+//		"img_height": "200",
+//		"link": lineLink,
+//		"desc": descContent,
+//		"title": shareTitle
+//	}, function (res) {
+//		//_report('timeline', res.err_msg);
+//	});
+//}
+//function shareWeibo() {
+//	WeixinJSBridge.invoke('shareWeibo', {
+//		"content": descContent,
+//		"url": lineLink
+//	}, function (res) {
+//		//_report('weibo', res.err_msg);
+//	});
+//}
