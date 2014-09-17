147bce8cec149545165a4aefe04312a8d588c903
wuhaolin <gwuhaolin@gmail.com>
2014-09-17 18:16:55 +0800

    增加了微信分享接口

--- baba/app.js
+++ baba/app.js
@@ -509,3 +509,56 @@ function commitInfo() {
     })
 }
 
+
+////////////////微信接口/////////////////
+var imgUrl = 'http://gwuhaolin.github.io/Game-5/duoduo/duoduo.png';
+var lineLink = 'http://gwuhaolin.github.io/Game-5/duoduo/index.html';
+var descContent = "挑战你的直觉,找出颜色最大的方块";
+var shareTitle = '挑战你的直觉,找出颜色最大的方块';
+var appid = 'wx5c6c73bc34fc424e';
+
+function shareFriend() {
+    WeixinJSBridge.invoke('sendAppMessage', {
+        "appid": appid,
+        "img_url": imgUrl,
+        "img_width": "200",
+        "img_height": "200",
+        "link": lineLink,
+        "desc": descContent,
+        "title": shareTitle
+    }, function (res) {
+    })
+}
+function shareTimeline() {
+    WeixinJSBridge.invoke('shareTimeline', {
+        "img_url": imgUrl,
+        "img_width": "200",
+        "img_height": "200",
+        "link": lineLink,
+        "desc": descContent,
+        "title": shareTitle
+    }, function (res) {
+    });
+}
+function shareWeibo() {
+    WeixinJSBridge.invoke('shareWeibo', {
+        "content": descContent,
+        "url": lineLink
+    }, function (res) {
+    });
+}
+// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+    // 发送给好友
+    WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+        shareFriend();
+    });
+    // 分享到朋友圈
+    WeixinJSBridge.on('menu:share:timeline', function (argv) {
+        shareTimeline();
+    });
+    // 分享到微博
+    WeixinJSBridge.on('menu:share:weibo', function (argv) {
+        shareWeibo();
+    });
+}, false);
